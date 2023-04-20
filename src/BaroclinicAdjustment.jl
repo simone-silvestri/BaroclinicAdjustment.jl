148bfc6e5f82a3ca4a7c534620899700dd3b4fde
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-04-20 19:14:18 -0400

    first commit

--- src/BaroclinicAdjustment.jl
+++ src/BaroclinicAdjustment.jl
@@ -1,5 +1,148 @@
 module BaroclinicAdjustment
 
-# Write your package code here.
+using Random
+using Oceananigans: getnamewrapper
+using Oceananigans.Units
+using GLMakie
+using Oceananigans
+
+include("horizontal_visc.jl")
+include("outputs.jl")
+
+function baroclinic_adjustment(resolution; horizontal_closure = nothing, momentum_advection = VectorInvariant())
+
+    # Architecture
+    arch = GPU()
+    
+    # Domain
+    Lz = 1kilometers     # depth [m]
+    Ny = Int(10 / resolution)
+    Nz = 100
+    stop_time = 200days
+    Δt = 10minutes
+
+    grid = LatitudeLongitudeGrid(arch;
+                                topology = (Periodic, Bounded, Bounded),
+                                size = (Ny, Ny, Nz), 
+                                longitude = (-5,   5),
+                                latitude = (-60, -50),
+                                z = (-Lz, 0),
+                                halo = (3, 3, 3))
+
+    coriolis = HydrostaticSphericalCoriolis()
+
+    Δy = 1000kilometers / Ny
+    vertical_closure = VerticalScalarDiffusivity(ν=1e-4, κ=1e-5)
+
+    closures = (vertical_closure, horizontal_closure)
+
+    @info "Building a model..."
+
+    model = HydrostaticFreeSurfaceModel(grid = grid,
+                                        coriolis = coriolis,
+                                        buoyancy = BuoyancyTracer(),
+                                        closure = closures,
+                                        tracers = :b,
+                                        momentum_advection = VectorInvariant(),
+                                        tracer_advection = WENO(),
+                                        free_surface = ImplicitFreeSurface())
+
+    @info "Built $model."
+
+    """
+    Linear ramp from 0 to 1 between -Δy/2 and +Δy/2.
+
+    For example:
+
+    y < y₀           => ramp = 0
+    y₀ < y < y₀ + Δy => ramp = y / Δy
+    y > y₀ + Δy      => ramp = 1
+    """
+    function ramp(λ, y, Δ)
+        gradient == "x" && return min(max(0, λ / Δ + 1/2), 1)
+        gradient == "y" && return min(max(0, (y - 45) / Δ + 1/2), 1)
+    end
+
+    # Parameters
+    N² = 4e-6 # [s⁻²] buoyancy frequency / stratification
+    M² = 8e-8 # [s⁻²] horizontal buoyancy gradient
+
+    Δy = 1 # degree
+    Δb = 100kilometers * Δy * M²
+
+    bᵢ(λ, y, z) = N² * z + Δb * ramp(λ, y, Δy)
+    
+    set!(model, b=bᵢ)
+
+    #####
+    ##### Simulation building
+    #####
+
+    simulation = Simulation(model; Δt, stop_time)
+
+    # add timestep wizard callback
+    wizard = TimeStepWizard(cfl=0.1, max_change=1.1, max_Δt=Δt)
+    simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(20))
+
+    # add progress callback
+    wall_clock = [time_ns()]
+
+    function print_progress(sim)
+        @printf("[%05.2f%%] i: %d, t: %s, wall time: %s, max(u): (%6.3e, %6.3e, %6.3e) m/s, next Δt: %s\n",
+                100 * (sim.model.clock.time / sim.stop_time),
+                sim.model.clock.iteration,
+                prettytime(sim.model.clock.time),
+                prettytime(1e-9 * (time_ns() - wall_clock[1])),
+                maximum(abs, sim.model.velocities.u),
+                maximum(abs, sim.model.velocities.v),
+                maximum(abs, sim.model.velocities.w),
+                prettytime(sim.Δt))
+
+        wall_clock[1] = time_ns()
+
+        return nothing
+    end
+
+    simulation.callbacks[:print_progress] = Callback(print_progress, IterationInterval(20))
+
+    filename = "experiment_$(viscosity_name(horizontal_closure))_$(advection_name(momentum_advection))"
+    
+    standard_outputs!(simulation, filename)
+
+    run!(simulation)
+
+    return nothing
+end
+
+viscosity_name(clo::ScalarBiharmonicDiffusivity) = typeof(clo.ν)
+viscosity_name(clo::ScalarDiffusivity)           = typeof(clo.ν)
+viscosity_name(clo) = typeof(clo)
+advection_name(adv) = getnamewrapper(adv.vorticity_scheme)
+
+function run_quarter_degree_simulations()
+
+    vi1 = VectorInvariant()
+    vi2 = VectorInvariant(vorticity_scheme = WENO(), divergence_scheme = WENO(), vertical_scheme = WENO())
+    vi3 = VectorInvariant(vorticity_scheme = WENO(), divergence_scheme = WENO(), vertical_scheme = WENO(), vorticity_stencil  = DefaultStencil())
+    vi4 = VectorInvariant(vorticity_scheme = WENO(), divergence_scheme = WENO(), vertical_scheme = WENO(), divergence_stencil = VelocityStencil())
+    vi5 = VectorInvariant(vorticity_scheme = WENO(order = 9), divergence_scheme = WENO(), vertical_scheme = WENO())
+
+    hi1 = nothing
+    hi2 = HorizontalScalarBiharmonicDiffusivity(ν = geometric_νhb, discrete_form = true, parameters = 5days)
+    hi3 = leith_viscosity(HorizontalFormulation())
+    hi3 = leith_laplacian_viscosity(HorizontalFormulation())
+    hi5 = smagorinski_viscosity(HorizontalFormulation())
+
+    advection_schemes   = [vi1, vi1, vi1, vi1, vi2, vi3, vi4, vi5]
+    horizontal_closures = [hi2, hi3, hi4, hi5, hi1, hi1, hi1, hi1]
+
+
+    for (momentum_advection, horizontal_closure) in zip(advection_schemes, horizontal_closures)
+        baroclinic_adjustment(1/4; momentum_advection, horizontal_closure)
+    end
+
+    return nothing
+end
+
 
 end
---
+++ src/outputs.jl
@@ -0,0 +1,133 @@
+using Oceananigans.Operators: ζ₃ᶠᶠᶜ
+using Oceananigans.AbstractOperations: KernelFunctionOperation
+
+using Oceananigans.Models: AbstractModel
+using Oceananigans.Distributed
+
+function standard_outputs!(simulation, output_prefix; overwrite_existing = true, 
+                                                      checkpoint_time    = 100days,
+                                                      snapshot_time      = 20days,
+                                                      surface_time       = 5days,
+                                                      average_time       = 10days,
+                                                      average_window     = average_time,
+                                                      average_stride     = 10)
+
+    model = simulation.model
+    grid  = model.grid
+
+    u, v, w = model.velocities
+    b = model.tracers.b
+
+    output_fields = (; u, v, w, b)
+
+    u2 = u^2
+    v2 = v^2
+    b2 = b^2
+    w2 = w^2
+    vb = v * b
+    ub = u * b
+    wb = w * b
+
+    ζ  = KernelFunctionOperation{Face, Face, Center}(ζ₃ᶠᶠᶜ, grid; computed_dependencies = (u, v))
+    ζ2 = ζ^2
+
+    averaged_fields = (; u, v, w, b, ζ, ζ2, u2, v2, w2, b2, ub, vb, wb)
+
+    simulation.output_writers[:snapshots] = JLD2OutputWriter(model, output_fields;
+                                                                  schedule = TimeInterval(snapshot_time),
+                                                                  filename = output_prefix * "_snapshots",
+                                                                  overwrite_existing)
+
+    simulation.output_writers[:surface_fields] = JLD2OutputWriter(model, output_fields;
+                                                                  schedule = TimeInterval(surface_time),
+                                                                  filename = output_prefix * "_surface",
+                                                                  indices = (:, :, grid.Nz),
+                                                                  overwrite_existing)
+
+    simulation.output_writers[:averaged_fields] = JLD2OutputWriter(model, averaged_fields;
+                                                                   schedule = AveragedTimeInterval(average_time, window=average_window, stride = average_stride),
+                                                                   filename = output_prefix * "_averages",
+                                                                   overwrite_existing)
+
+    simulation.output_writers[:checkpointer] = Checkpointer(model;
+                                                            schedule = TimeInterval(checkpoint_time),
+                                                            prefix = output_prefix * "_checkpoint",
+                                                            overwrite_existing)
+
+    return nothing
+end
+
+function checkpoint_outputs!(simulation, output_prefix; overwrite_existing = true, checkpoint_time = 100days)
+
+    model = simulation.model
+
+    simulation.output_writers[:checkpointer] = Checkpointer(model;
+                                                            schedule = TimeInterval(checkpoint_time),
+                                                            prefix = output_prefix * "_checkpoint",
+                                                            overwrite_existing)
+
+    return nothing
+end
+
+function reduced_outputs!(simulation, output_prefix; overwrite_existing = true, 
+                                                     checkpoint_time    = 100days,
+                                                     snapshot_time      = 30days,
+                                                     surface_time       = 1days,
+                                                     bottom_time        = 1days)
+
+    model = simulation.model
+    grid  = model.grid
+
+    u, v, w = model.velocities
+    b = model.tracers.b
+
+    output_fields = (; u, v, w, b)
+
+    simulation.output_writers[:snapshots] = JLD2OutputWriter(model, output_fields;
+                                                                schedule = TimeInterval(snapshot_time),
+                                                                filename = output_prefix * "_snapshots",
+                                                                overwrite_existing)
+   
+
+    simulation.output_writers[:surface_fields] = JLD2OutputWriter(model, output_fields;
+                                                                    schedule = TimeInterval(surface_time),
+                                                                    filename = output_prefix * "_surface",
+                                                                    indices = (:, :, grid.Nz),
+                                                                    overwrite_existing)
+                                                                                                                                
+    simulation.output_writers[:bottom_fields] = JLD2OutputWriter(model, output_fields;
+                                                                    schedule = TimeInterval(bottom_time),
+                                                                    filename = output_prefix * "_bottom",
+                                                                    indices = (:, :, 2),
+                                                                    overwrite_existing)
+    
+    simulation.output_writers[:checkpointer] = Checkpointer(model;
+                                                            schedule = TimeInterval(checkpoint_time),
+                                                            prefix = output_prefix * "_checkpoint",
+                                                            overwrite_existing)
+
+end                                                 
+
+import Oceananigans.OutputWriters: set_time_stepper_tendencies!
+
+function set_time_stepper_tendencies!(timestepper, file, model_fields)
+    for name in propertynames(model_fields)
+        # Tendency "n"
+        try
+            parent_data = file["timestepper/Gⁿ/$name/data"]
+
+            tendencyⁿ_field = timestepper.Gⁿ[name]
+            copyto!(tendencyⁿ_field.data.parent, parent_data)
+
+            # Tendency "n-1"
+            parent_data = file["timestepper/G⁻/$name/data"]
+
+            tendency⁻_field = timestepper.G⁻[name]
+            copyto!(tendency⁻_field.data.parent, parent_data)
+        catch
+            @warn "Could not restore $name tendency from checkpoint."
+        end
+    end
+
+    return nothing
+end
