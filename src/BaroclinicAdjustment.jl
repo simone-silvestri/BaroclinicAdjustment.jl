031eb24cf4365f80008ac599a3bd76de1d75039e
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-04-28 09:54:01 -0400

    rectilinear adjustement

--- src/BaroclinicAdjustment.jl
+++ src/BaroclinicAdjustment.jl
@@ -44,8 +44,111 @@ function barotropic_substeps(Δt, grid, gravitational_acceleration)
 
     return  Base.Int(ceil(2 * Δt / (0.7 / wave_speed * Δ)))
 end
+
+function baroclinic_adjustment_rectilinear(resolution, filename; arch = GPU(), 
+                                           horizontal_closure = nothing,
+                                           momentum_advection = VectorInvariant(), 
+                                           xdirection = true)
+
+    # Domain
+    Lz = 1kilometers     # depth [m]
+    Ny = Base.Int(2000kilometers ÷ (resolution * 100kilometers))
+    Nz = 100
+    stop_time = 200days
+    Δt = 2.5minutes
+
+    if xdirection 
+        grid = RectilinearGrid(arch;
+                               topology = (Periodic, Bounded, Bounded),
+                               size = (Ny, Ny, Nz), 
+                               x = (0, 2000kilometers),
+                               x = (0, 2000kilometers),
+                               z = (-Lz, 0),
+                               halo = (6, 6, 6))
+    else
+        grid = RectilinearGrid(arch;
+                               topology = (Flat, Bounded, Bounded),
+                               size = (Ny, Nz), 
+                               latitude = (-60, -40),
+                               z = (-Lz, 0),
+                               halo = (6, 6))
+    end
+
+    coriolis = BetaPlane(latitude = -45)
+
+    Δy = 1000kilometers / Ny
+    vertical_closure = ConvectiveAdjustmentVerticalDiffusivity(convective_κz = one(grid),
+                                                               convective_νz = zero(grid),
+                                                               background_κz = 1e-6,
+                                                               background_νz = 1e-4)
+
+    closures = (vertical_closure, horizontal_closure)
+
+    @info "Building a model..."
+
+    model = HydrostaticFreeSurfaceModel(; grid,
+                                          coriolis,
+                                          buoyancy = BuoyancyTracer(),
+                                          closure = closures,
+                                          tracers = :b,
+                                          momentum_advection,
+                                          tracer_advection = WENO(),
+                                          free_surface = ImplicitFreeSurface())
+
+    @info "Built $model."
+
+    ramp(y, Δy) = min(max(0, y/Δy + 1/2), 1)
+
+    # Parameters
+    N² = 4e-6 # [s⁻²] buoyancy frequency / stratification
+
+    Δb = 0.06
+    Δy = 50kilometers
+        
+    bᵢ(x, y, z) = N² * z + Δb * ramp(y, Δy)
+
+    set!(model, b=bᵢ)
+    set_geostrophic_velocity!(model.velocities.u, model.tracers.b, model.coriolis)
+
+    #####
+    ##### Simulation building
+    #####
+
+    simulation = Simulation(model; Δt, stop_time)
+
+    # add timestep wizard callback
+    wizard = TimeStepWizard(cfl=0.1, max_change=1.1, max_Δt=Δt)
+    simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(20))
+
+    # add progress callback
+    wall_clock = [time_ns()]
+
+    function print_progress(sim)
+        @printf("[%05.2f%%] i: %d, t: %s, wall time: %s, max(u): (%6.3e, %6.3e, %6.3e) m/s, next Δt: %s\n",
+        100 * (sim.model.clock.time / sim.stop_time),
+        sim.model.clock.iteration,
+        prettytime(sim.model.clock.time),
+        prettytime(1e-9 * (time_ns() - wall_clock[1])),
+        maximum(abs, sim.model.velocities.u),
+        maximum(abs, sim.model.velocities.v),
+        maximum(abs, sim.model.velocities.w),
+        prettytime(sim.Δt))
+
+        wall_clock[1] = time_ns()
+
+        return nothing
+    end
+
+    simulation.callbacks[:print_progress] = Callback(print_progress, IterationInterval(20))
+
+    reduced_outputs!(simulation, filename)
+
+    run!(simulation)
+
+    return nothing
+end
     
-function baroclinic_adjustment(resolution, filename; arch = GPU(), 
+function baroclinic_adjustment_latlong(resolution, filename; arch = GPU(), 
                                                      horizontal_closure = nothing,
                                                      momentum_advection = VectorInvariant(), 
                                                      xdirection = true)
@@ -195,7 +298,7 @@ function run_eight_degree_simulations(trailing_character = "_weaker")
     names = add_trailing_characters.(names, Ref(trailing_character))
     
     for (momentum_advection, horizontal_closure, name) in zip(advection_schemes, horizontal_closures, names)
-        baroclinic_adjustment(1/8, name; momentum_advection, horizontal_closure)
+        baroclinic_adjustment_latlong(1/8, name; momentum_advection, horizontal_closure)
     end
 
     return nothing
@@ -214,14 +317,14 @@ function run_high_res_simulation(trailing_character = "_weaker")
     names = add_trailing_characters.(names, Ref(trailing_character))
 
     for (momentum_advection, horizontal_closure, name) in zip(advection_schemes, horizontal_closures, names)
-        baroclinic_adjustment(1/50, name; momentum_advection, horizontal_closure)
+        baroclinic_adjustment_latlong(1/50, name; momentum_advection, horizontal_closure)
     end
 
     return nothing
 end
 
 run_2d_flat_simulation(trailing_character = "_weaker") = 
-    baroclinic_adjustment(1/8, add_trailing_characters("2dsim", trailing_character);
+    baroclinic_adjustment_latlong(1/8, add_trailing_characters("2dsim", trailing_character);
                           horizontal_closure = leith_viscosity(HorizontalFormulation()),
                           xdirection = false)
 
