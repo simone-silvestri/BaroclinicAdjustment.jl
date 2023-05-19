740e1b1300d643a07c0fe27add50583e9cf5c9a2
simone-silvestri <silvestri.simone0@gmail.com>
2023-05-19 09:50:42 -0400

    correct qg Leith

--- src/qg_leith_viscosity.jl
+++ src/qg_leith_viscosity.jl
@@ -27,7 +27,7 @@ using Oceananigans.Coriolis: fᶠᶠᵃ
 using Oceananigans.Operators
 using Oceananigans.BuoyancyModels: ∂x_b, ∂y_b, ∂z_b 
 
-using Oceananigans.Operators: ℑxyzᶜᶜᶠ, ℑyzᵃᶜᶠ, ℑxyzᶜᶜᶠ, ℑxzᶜᵃᶠ, Δxᶜᶜᶜ, Δyᶜᶜᶜ
+using Oceananigans.Operators: ℑxyzᶜᶜᶠ, ℑyzᵃᶜᶠ, ℑxzᶜᵃᶠ, Δxᶜᶜᶜ, Δyᶜᶜᶜ
 
 struct QGLeithViscosity{A, M, S} <: AbstractScalarDiffusivity{ExplicitTimeDiscretization, HorizontalFormulation}
     C :: A
@@ -39,7 +39,8 @@ QGLeithViscosity(; C=1.0, isopycnal_model=SmallSlopeIsopycnalTensor(), slope_lim
     QGLeithViscosity(C, isopycnal_model, slope_limiter) 
 
 DiffusivityFields(grid, tracer_names, bcs, ::QGLeithViscosity) = 
-                (; νₑ = CenterField(grid), )
+                (; νₑ = CenterField(grid),
+                   Ld = Field{Center, Center, Nothing}(grid))
 
 @inline function abs²_∇h_ζ(i, j, k, grid, coriolis, fields)
 
@@ -61,34 +62,54 @@ end
 end
 
 @inline ∂yb_times_f2_div_N2(i, j, k, grid, coriolis, buoyancy, tracers) = ℑxyzᶜᶜᶠ(i, j, k, grid, fᶠᶠᵃ, coriolis) / 
-                                                                          max(0, ∂z_b(i, j, k, grid, buoyancy, tracers)) *
+                                                                          max(1e-20, ∂z_b(i, j, k, grid, buoyancy, tracers)) *
                                                                           ℑyzᵃᶜᶠ(i, j, k, grid, ∂y_b, buoyancy, tracers)
 
 @inline ∂xb_times_f2_div_N2(i, j, k, grid, coriolis, buoyancy, tracers) = ℑxyzᶜᶜᶠ(i, j, k, grid, fᶠᶠᵃ, coriolis) / 
-                                                                          max(0, ∂z_b(i, j, k, grid, buoyancy, tracers))  *
+                                                                          max(1e-20, ∂z_b(i, j, k, grid, buoyancy, tracers))  *
                                                                           ℑxzᶜᵃᶠ(i, j, k, grid, ∂x_b, buoyancy, tracers)
 
 @inline function abs²_∇h_q(i, j, k, grid, coriolis, buoyancy, tracers)
 
-    ∂zqx = min(∂zᶜᶜᶜ(i, j, k, grid, ∂xb_times_f2_div_N2, coriolis, buoyancy, tracers), 1e-4)
-    ∂zqy = min(∂zᶜᶜᶜ(i, j, k, grid, ∂yb_times_f2_div_N2, coriolis, buoyancy, tracers), 1e-4)
+    ∂zqx = ∂zᶜᶜᶜ(i, j, k, grid, ∂xb_times_f2_div_N2, coriolis, buoyancy, tracers)
+    ∂zqy = ∂zᶜᶜᶜ(i, j, k, grid, ∂yb_times_f2_div_N2, coriolis, buoyancy, tracers)
 
     return (∂zqx^2 + ∂zqy^2)
 end
 
 "Return the filter width for a Leith Diffusivity on a general grid."
-@inline Δᶜᶜᶜ(i, j, k, grid) =  2 * (1 / (1 / Δxᶜᶜᶜ(i, j, k, grid)^2 + 1 / Δyᶜᶜᶜ(i, j, k, grid)^2))
+@inline Δ²ᶜᶜᶜ(i, j, k, grid) =  2 * (1 / (1 / Δxᶜᶜᶜ(i, j, k, grid)^2 + 1 / Δyᶜᶜᶜ(i, j, k, grid)^2))
 
-@kernel function calculate_qgleith_viscosity!(ν, grid, closure, coriolis, buoyancy, velocities, tracers)
+@kernel function calculate_qgleith_viscosity!(ν, grid, Ld, closure, velocities, tracers, buoyancy, coriolis)
     i, j, k = @index(Global, NTuple)
 
     ∂ζ² =  abs²_∇h_ζ(i, j, k, grid, coriolis, velocities)
     ∂δ² =  abs²_∇h_δ(i, j, k, grid, velocities)
     ∂q² =  abs²_∇h_q(i, j, k, grid, coriolis, buoyancy, tracers)
 
+    A  = Δ²ᶜᶜᶜ(i, j, k, grid)
+
+    Bu  = A / Ld[i, j, 1]^2
+    ∂Q² = min(∂ζ² + ∂q², ∂ζ² * (1 + 1 / Bu))
+
     C = closure.C
 
-    @inbounds ν[i, j, k] = (C * Δᶜᶜᶜ(i, j, k, grid) / π)^3 * sqrt(∂ζ² + ∂δ² + ∂q²) 
+    @inbounds ν[i, j, k] = (C *  A/ π)^(3 / 2) * sqrt(∂Q² + ∂δ²) 
+end
+
+@inline _deformation_radius(i, j, k, grid, C, buoyancy, coriolis) = sqrt(max(0, ∂z_b(i, j, k, grid, buoyancy, C))) / π /
+                                                                         abs(ℑxyᶜᶜᵃ(i, j, k, grid, fᶠᶠᵃ, coriolis))
+
+@kernel function calculate_deformation_radius!(Ld, grid, tracers, buoyancy, coriolis)
+    i, j = @index(Global, NTuple)
+
+    @inbounds begin
+        Ld[i, j, 1] = 0
+
+        @unroll for k in 1:grid.Nz
+            Ld[i, j, 1] += Δzᶜᶜᶠ(i, j, k, grid) * _deformation_radius(i, j, k, grid, tracers, buoyancy, coriolis)
+        end
+    end
 end
 
 function calculate_diffusivities!(diffusivity_fields, closure::QGLeithViscosity, model)
@@ -99,9 +120,12 @@ function calculate_diffusivities!(diffusivity_fields, closure::QGLeithViscosity,
     buoyancy = model.buoyancy
     coriolis = model.coriolis
 
+    launch!(arch, grid, :xy, 
+            calculate_deformation_radius!, diffusivity_fields.Ld, grid, tracers, buoyancy, coriolis)
+
     launch!(arch, grid, :xyz,
             calculate_qgleith_viscosity!,
-            diffusivity_fields.νₑ, grid, closure, coriolis, buoyancy, velocities, tracers)
+            diffusivity_fields.νₑ, diffusivity_fields.Ld, grid, closure, velocities, tracers, buoyancy, coriolis)
 
     return nothing
 end
