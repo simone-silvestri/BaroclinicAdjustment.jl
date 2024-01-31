69da3ee0486fd012d90df65de3e23a9591c1c205
simone-silvestri <silvestri.simone0@gmail.com>
2024-01-31 16:15:14 -0500

    added backscattering

--- src/Parameterizations/energy_backscatter.jl
+++ src/Parameterizations/energy_backscatter.jl
@@ -1,6 +1,12 @@
 using Oceananigans.Operators
+using Oceananigans.Advection: div_Uc
 using Oceananigans.Coriolis: fᶠᶠᵃ
-using Oceananigans.TurbulenceClosures: biharmonic_mask_x, biharmonic_mask_y, ∂x_∇²h_cᶠᶜᶜ, ∂y_∇²h_cᶜᶠᶜ
+using Oceananigans.TurbulenceClosures: 
+                    biharmonic_mask_x, 
+                    biharmonic_mask_y, 
+                    ∂ⱼ_τ₁ⱼ, ∂ⱼ_τ₂ⱼ,
+                    ∂x_∇²h_cᶠᶜᶜ, 
+                    ∂y_∇²h_cᶜᶠᶜ
 
 struct EnergyBackScatter{FT} <: AbstractScalarDiffusivity{ExplicitTimeDiscretization, HorizontalFormulation, 2}
     C₄ :: FT
@@ -51,8 +57,8 @@ function compute_diffusivities!(diffusivity_fields, closure::EBS, model; paramet
     velocities = model.velocities
     advection = model.advection.b
     coriolis = model.coriolis
-    Δt = model.clock.time - diffusivities.previous_compute_time[]
-    diffusivities.previous_compute_time[] = model.clock.time
+    Δt = model.clock.time - diffusivity_fields.previous_compute_time[]
+    diffusivity_fields.previous_compute_time[] = model.clock.time
 
     launch!(arch, grid, parameters, 
             _advance_tke!, diffusivity_fields,
@@ -69,8 +75,12 @@ end
 
 using Oceananigans.Operators
 
-@kernel function _advance_tke!(diffusivities, grid, velocities, advection, closure, Δt)
+@inline ε_x(i, j, k, grid, u, args...) = @inbounds u[i, j, k] * ∂ⱼ_τ₁ⱼ(i, j, k, grid, args...)
+@inline ε_y(i, j, k, grid, v, args...) = @inbounds v[i, j, k] * ∂ⱼ_τ₂ⱼ(i, j, k, grid, args...)
+
+@kernel function _advance_tke!(diffusivities, grid, clock, velocities, advection, closure, Δt)
     i, j, k = @index(Global, NTuple)
+    u, v, _ = velocities
 
     FT = eltype(grid)
     α = convert(FT, 1.6)
@@ -80,11 +90,14 @@ using Oceananigans.Operators
 
     e  = diffusivities.e
     G⁻ = diffusivities.G⁻
-    Gⁿ = - div_Uc(i, j, k, grid, advection, velocities, e) 
-         - ∇_dot_qᵉ(i, j, k, grid, closure, diffusivities, e)
-         - Cᴰ * e[i, j, k]
 
-    @inbounds e[i, j, k] += Δt * (α * G - β * G⁻[i, j, k])
+    εˣ = ℑxᶜᵃᵃ(i, j, k, grid, ε_x, u, closure, diffusivities, clock, velocities, nothing)
+    εʸ = ℑxᶜᵃᵃ(i, j, k, grid, ε_y, v, closure, diffusivities, clock, velocities, nothing)
+
+    Gⁿ = - div_Uc(i, j, k, grid, advection, velocities, e) 
+         - Cᴰ * e[i, j, k] + (εˣ + εʸ) / 2
+
+    @inbounds e[i, j, k] += Δt * (α * Gⁿ - β * G⁻[i, j, k])
     @inbounds G⁻[i, j, k] = Gⁿ
 end
 
@@ -130,8 +143,8 @@ end
 @inline _backscatter_diffusive_flux_x(args...) = backscatter_diffusive_flux_x(args...)
 @inline _backscatter_diffusive_flux_y(args...) = backscatter_diffusive_flux_y(args...)
 
-@inline backscatter_diffusive_flux_x(i, j, k, grid, K, e) = @inbounds - K.ν₂[i, j, k] * ∂xᶠᶜᶜ(i, j, k, grid, e) + K.ν₄[i, j, k] * biharmonic_mask_x(i, j, k, grid, ∂x_∇²h_cᶠᶜᶜ, c)
-@inline backscatter_diffusive_flux_y(i, j, k, grid, K, e) = @inbounds - K.ν₂[i, j, k] * ∂yᶜᶠᶜ(i, j, k, grid, e) + K.ν₄[i, j, k] * biharmonic_mask_y(i, j, k, grid, ∂y_∇²h_cᶜᶠᶜ, c)
+@inline backscatter_diffusive_flux_x(i, j, k, grid, K, e) = @inbounds - K.ν₂[i, j, k] * ∂xᶠᶜᶜ(i, j, k, grid, e) + K.ν₄[i, j, k] * biharmonic_mask_x(i, j, k, grid, ∂x_∇²h_cᶠᶜᶜ, e)
+@inline backscatter_diffusive_flux_y(i, j, k, grid, K, e) = @inbounds - K.ν₂[i, j, k] * ∂yᶜᶠᶜ(i, j, k, grid, e) + K.ν₄[i, j, k] * biharmonic_mask_y(i, j, k, grid, ∂y_∇²h_cᶜᶠᶜ, e)
 
 @inline diffusive_flux_x(i, j, k, grid, ::EBS, K, ::Val{id}, c, clk, fields, b) where id = zero(grid)
 @inline diffusive_flux_y(i, j, k, grid, ::EBS, K, ::Val{id}, c, clk, fields, b) where id = zero(grid)
